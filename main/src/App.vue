5085d2f331b8082954e0df2fdb756681b5024f7a
Taxi_tabby <rkdmf0000@gmail.com>
2023-04-28 03:59:57 +0000

    xxx

--- main/src/App.vue
+++ main/src/App.vue
@@ -1,27 +1,19 @@
+<script setup lang="ts">
+
+import frame from './components/v1.vue';
+
+</script>
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <frame />
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    *{
+        font-size:0.98em;box-sizing:border-box;}
 }
 </style>
---
+++ main/src/components/v1.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div>
+            video_id : <input type="text" v-model="temp.video_id" /><br />
+            loop : <input type="number" v-model.number="temp.loop" /><br />
+            <button @click="applyConfig">Apply</button>
+            <button @click="playCurrentVideo">Play</button>
+            <button @click="stopCurrentVideo">Stop</button>
+            <button @click="pauseCurrentVideo">Pause</button>
+        </div>
+        <YoutubeVue3 ref="youtube" :videoid="play.video_id" :loop="play.loop" :width="480" :height="320" @ended="onEnded"
+            @paused="onPaused" @played="onPlayed" />
+    </div>
+</template>
+  
+<script lang="js">
+import { YoutubeVue3 } from 'youtube-vue3'
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            temp: { video_id: "3P1CnWI62Ik", loop: 1 },
+            play: { video_id: "3P1CnWI62Ik", loop: 1 },
+        }
+    },
+    components: {
+        YoutubeVue3,
+    },
+    methods: {
+        applyConfig() {
+            this.play = Object.assign(this.play, this.temp)
+        },
+        playCurrentVideo() {
+            this.$refs.youtube.player.playVideo();
+        },
+        stopCurrentVideo() {
+            this.$refs.youtube.player.stopVideo();
+        },
+        pauseCurrentVideo() {
+            this.$refs.youtube.player.pauseVideo();
+        },
+        onEnded() {
+            console.log("## OnEnded")
+            console.log(`path : ${location.pathname}  /  search : ${location.search}`);
+        },
+        onPaused() {
+            console.log("## OnPaused")
+        },
+        onPlayed() {
+            console.log("## OnPlayed")
+        },
+    },
+}
+</script>
