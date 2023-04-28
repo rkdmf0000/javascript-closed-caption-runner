2a22ff8382062fdb67e446f65c4fe919835f74ed
Taxi_tabby <rkdmf0000@gmail.com>
2023-04-28 04:21:21 +0000

    XXX

--- main/src/components/v1.vue
+++ main/src/components/v1.vue
@@ -1,14 +1,21 @@
 <template>
     <div>
         <div>
+
             video_id : <input type="text" v-model="temp.video_id" /><br />
             loop : <input type="number" v-model.number="temp.loop" /><br />
+            example : 3P1CnWI62Ik <br />
+
             <button @click="applyConfig">Apply</button>
             <button @click="playCurrentVideo">Play</button>
             <button @click="stopCurrentVideo">Stop</button>
             <button @click="pauseCurrentVideo">Pause</button>
+            
         </div>
-        <YoutubeVue3 ref="youtube" :videoid="play.video_id" :loop="play.loop" :width="480" :height="320" @ended="onEnded"
+
+        <br />
+
+        <YoutubeVue3 ref="youtube" :controls="1" :videoid="play.video_id" :loop="play.loop" :width="480" :height="320" @ended="onEnded"
             @paused="onPaused" @played="onPlayed" />
     </div>
 </template>
@@ -30,6 +37,7 @@ export default {
     methods: {
         applyConfig() {
             this.play = Object.assign(this.play, this.temp)
+            
         },
         playCurrentVideo() {
             this.$refs.youtube.player.playVideo();
